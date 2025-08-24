c3d442358c00dee4c4b983d64e1b7f7c0f3fe133
Kate <work@localcc.cc>
2025-08-24 17:09:28 +0200

    enable auto gc

--- common/nix.nix
+++ common/nix.nix
@@ -7,4 +7,10 @@
     inputs.agenix.overlays.default
     inputs.rust-overlay.overlays.default
   ];
+
+  # auto gc
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 30d";
+  };
 }
