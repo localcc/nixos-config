1e79e18a48a3e25b2d5444f357803d67cff3be88
kate <work@localcc.cc>
2026-01-03 01:19:32 +0100

    optional wifi

--- common/nixos/network.nix
+++ common/nixos/network.nix
@@ -1,45 +1,60 @@
 {
   config,
   inputs,
+  lib,
   ...
 }:
+let
+  wifi = config.wifi;
+in
 {
-  networking.networkmanager.enable = true;
-
-  # mdns
-  services.avahi.enable = false;
-  services.resolved.enable = true;
-
-  # ssh
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
+  options = {
+    wifi = {
+      enable = lib.mkEnableOption "WiFi";
     };
   };
 
-  # wifi
-  age.secrets.wifi-home.file = (inputs.self + /secrets/wifi-home.age);
-  networking.networkmanager.ensureProfiles = {
-    environmentFiles = [
-      config.age.secrets.wifi-home.path
-    ];
+  config = lib.mkMerge [
+    (lib.mkIf wifi.enable {
+      # wifi
+      age.secrets.wifi-home.file = (inputs.self + /secrets/wifi-home.age);
+      networking.networkmanager.ensureProfiles = {
+        environmentFiles = [
+          config.age.secrets.wifi-home.path
+        ];
 
-    profiles = {
-      Home = {
-        connection = {
-          id = "$HOME_SSID";
-          type = "wifi";
-        };
-        wifi = {
-          mode = "infrastructure";
-          ssid = "$HOME_SSID";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$HOME_PSK";
+        profiles = {
+          Home = {
+            connection = {
+              id = "$HOME_SSID";
+              type = "wifi";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "$HOME_SSID";
+            };
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+              psk = "$HOME_PSK";
+            };
+          };
         };
       };
-    };
-  };
+    })
+    (lib.mkIf true {
+      networking.networkmanager.enable = true;
+
+      # mdns
+      services.avahi.enable = lib.mkDefault false;
+      services.resolved.enable = true;
+
+      # ssh
+      services.openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+        };
+      };
+    })
+  ];
 }
