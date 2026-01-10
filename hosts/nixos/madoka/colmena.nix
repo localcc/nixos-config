886a0fef5c53062233e54d8ab58c68b9085a75fd
localcc <work@localcc.cc>
2026-01-10 16:53:20 +0100

    fix(madoka): samba

--- hosts/nixos/madoka/colmena.nix
+++ hosts/nixos/madoka/colmena.nix
@@ -4,7 +4,7 @@
 {
   # colmena
   deployment = {
-    targetHost = "192.168.0.56";
+    targetHost = "192.168.0.16";
     targetPort = 22;
     replaceUnknownProfiles = false;
     buildOnTarget = false;
