44a2211f3864149924c5d6978ea1954ac9558a76
kate <work@localcc.cc>
2026-01-11 15:45:20 +0100

    feat(madoka): switch media-server to nix

--- hosts/nixos/madoka/services/tailscale.nix
+++ hosts/nixos/madoka/services/tailscale.nix
@@ -4,7 +4,7 @@
   ...
 }:
 {
-  age.secrets.madoka-tailscale-key.file = (inputs.self + /secrets/madoka-tailscale-key.age);
+  age.secrets.madoka-tailscale-key.file = (inputs.secrets + /madoka-tailscale-key.age);
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets.madoka-tailscale-key.path;
