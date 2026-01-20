4b776933dd842550ef91d891a6d614606406fc54
Kate <work@localcc.cc>
2026-01-20 11:54:21 +0100

    feat: ambra wifi

--- common/nixos/network.nix
+++ common/nixos/network.nix
@@ -18,9 +18,12 @@ in
     (lib.mkIf wifi.enable {
       # wifi
       age.secrets.wifi-home.file = (inputs.secrets + /wifi-home.age);
+      age.secrets.wifi-ambra.file = (inputs.secrets + /wifi-ambra.age);
+      
       networking.networkmanager.ensureProfiles = {
         environmentFiles = [
           config.age.secrets.wifi-home.path
+          config.age.secrets.wifi-ambra.path
         ];
 
         profiles = {
@@ -36,6 +39,34 @@ in
             wifi-security = {
               key-mgmt = "wpa-psk";
               psk = "$HOME_PSK";
+            };
+          };
+          Ambra1 = {
+            connection = {
+              id = "$AMBRA1_SSID";
+              type = "wifi";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "$AMBRA1_SSID";
+            };
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+              psk = "$AMBRA_PSK";
+            };
+          };
+          Ambra2 = {
+            connection = {
+              id = "$AMBRA2_SSID";
+              type = "wifi";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "$AMBRA2_SSID";
+            };
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+              psk = "$AMBRA_PSK";
             };
           };
         };
