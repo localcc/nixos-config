c5e712197c1f9ea74f250026973dce8d68ce1a4b
Kate <work@localcc.cc>
2026-01-20 11:54:21 +0100

    feat: enable obs virtual camera

--- common/nixos/desktop.nix
+++ common/nixos/desktop.nix
@@ -33,6 +33,7 @@ in
     programs.localsend.enable = true;
     programs.obs-studio = {
       enable = true;
+      enableVirtualCamera = true;
       package = pkgs.obs-studio.override { cudaSupport = true; };
     };
 
