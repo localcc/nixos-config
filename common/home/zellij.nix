def6b6811b40defcbf06ec6efba8cc59fd5fb71b
Kate <work@localcc.cc>
2026-01-20 11:54:21 +0100

    chore: more zellij customization

--- common/home/zellij.nix
+++ common/home/zellij.nix
@@ -7,11 +7,14 @@
     extraConfig = ''
       keybinds {
         unbind "Ctrl o"
+        unbind "Ctrl s"
         normal {
           bind "Ctrl PageUp" { GoToPreviousTab; }
           bind "Ctrl PageDown" { GoToNextTab; }
           bind "Ctrl Shift t" { NewTab; }
           bind "Ctrl Shift w" { CloseTab; }
+          bind "Ctrl Shift PageUp" { MoveTab "Left"; }
+          bind "Ctrl Shift PageDown" { MoveTab "Right"; }
         }
       }
     '';
