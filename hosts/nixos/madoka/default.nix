9522cf669fb37b3292c148d9be381f02dc6d1d31
Kate <work@localcc.cc>
2026-01-20 11:54:21 +0100

    fix(madoka): don't enable tailscale twice

--- hosts/nixos/madoka/default.nix
+++ hosts/nixos/madoka/default.nix
@@ -26,13 +26,6 @@ in
     ./hardware-configuration.nix
   ];
 
-  age.secrets.madoka-tailscale-key.file = (inputs.secrets + /madoka-tailscale-key.age);
-  services.tailscale = {
-    enable = true;
-    authKeyFile = config.age.secrets.madoka-tailscale-key.path;
-    useRoutingFeatures = "server";
-  };
-
   # Boot
   boot.kernelPackages = latestKernelPackage;
   boot.supportedFilesystems = [ "zfs" ];
