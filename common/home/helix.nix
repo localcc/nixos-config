627c0bb48407ad9f850e4df93c3ebcccf301e711
Kate <work@localcc.cc>
2026-01-20 11:54:21 +0100

    feat: helix customizations

--- common/home/helix.nix
+++ common/home/helix.nix
@@ -5,6 +5,26 @@
 }:
 let
   cfg = config.helix;
+
+  # rosePineTheme = pkgs.fetchFromGitHub {
+  #   owner = "rose-pine";
+  #   repo = "helix";
+  #   rev = "2e40340c21e2f1f47e4b2b368471900a7bf5263b";
+  #   hash = "sha256-HyPeq5eghE8eMswLmV/uN6CF8i943LYCkOBy4MNGxeU=";
+  # };
+
+  # themeFiles = builtins.listToAttrs (
+  #   map
+  #     (f: {
+  #       name = lib.removeSuffix ".toml" f;
+  #       value = builtins.readFile (rosePineTheme + "/${f}");
+  #     })
+  #     (
+  #       builtins.filter (a: lib.hasSuffix ".toml" a) (
+  #         lib.mapAttrsToList (k: v: k) (builtins.readDir rosePineTheme)
+  #       )
+  #     )
+  # );
 in
 {
   options = {
@@ -18,12 +38,23 @@ in
       enable = true;
       settings = {
         editor.lsp.display-inlay-hints = true;
+        theme = "horizon-dark-kate";
       };
       languages.language = [
         {
           name = "rust";
         }
       ];
+      themes = {
+        "horizon-dark-kate" = ''
+          inherits = "horizon-dark"
+
+          [palette]
+          bg = "#191b23"
+          light-gray = "#898ca8"
+          gray = "#44485e"
+        '';
+      };
     };
   };
 }
