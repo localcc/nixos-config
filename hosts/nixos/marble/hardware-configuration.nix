74a84de21eb9cdd9cc01932845230e57e23ccda5
Kate <work@localcc.cc>
2026-01-20 11:54:21 +0100

    fix(marble): build

--- hosts/nixos/marble/hardware-configuration.nix
+++ hosts/nixos/marble/hardware-configuration.nix
@@ -29,6 +29,7 @@
   boot.kernelParams = [
     "amd_iommu=on"
     "nvidia.NVreg_RegistryDwords=EnableBrightnessControl=1"
+  ];
   boot.kernelPatches = [
     {
       name = "dgpu-runtime-enable";
