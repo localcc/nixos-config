4aa2983c8b4d97abf69535d221d4d7fbcca51413
Kate <work@localcc.cc>
2026-09-01 18:30:46 +0200

    feat: add zellai shortcut

--- common/home/zellij.nix
+++ common/home/zellij.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 {
@@ -27,6 +28,48 @@
           pane
         }
       '';
+      ai = ''
+        layout {
+          default_tab_template {
+            pane size=1 borderless=true {
+              plugin location="tab-bar"
+            }
+            children
+          }
+        
+          tab name="Helix" {
+            pane {
+              command "nix"
+              args "develop" "-c" "nu" "-c" "hx"
+              close_on_exit true
+            }
+          }
+          tab name="Terminal" {
+            pane {
+              command "nix"
+              args "develop" "-c" "nu"
+              close_on_exit true
+            }
+          }
+          tab {
+            pane {
+              command "nix"
+              args "develop" "-c" "nu" "-c" "opencode" "--auto"
+              close_on_exit true
+            }
+          }
+        }
+      '';
     };
   };
+
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "zellai";
+      runtimeInputs = [ pkgs.zellij ];
+      text = ''
+        zellij -l ai
+      '';
+    })
+  ];
 }
